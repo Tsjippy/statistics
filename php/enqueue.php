a760696bf1122a68954a40e4cde1fcb25656a620
Tsjippy <ewaldha@hotmail.com>
2024-10-11 07:20:00 +0100

    changed: redering of asset urls

--- php/enqueue.php
+++ php/enqueue.php
@@ -4,5 +4,5 @@ use SIM;
 
 add_action('wp_enqueue_scripts', function(){
     //Load js
-    wp_enqueue_script('sim_statistics_script', plugins_url('js/statistics.min.js', __DIR__), array(), MODULE_VERSION,true);
+    wp_enqueue_script('sim_statistics_script', SIM\pathToUrl(MODULE_PATH.'js/statistics.min.js'), array(), MODULE_VERSION,true);
 });

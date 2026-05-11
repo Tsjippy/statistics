07b9394d331dda51ad589ef85a5b4574e8932edc
Tsjippy <ewaldha@hotmail.com>
2026-05-11 21:17:22 +0200

    updated: block js

--- blocks/statistics/build/index.asset.php
+++ blocks/statistics/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a0efed58330a5ecc99b6');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'ab026b97ee5baf7368fa');

4aa51f157cbc22aa710213749ce04bd1c5854b60
Tsjippy <ewaldha@hotmail.com>
2026-07-08 15:20:51 +0200

    changed: statistics block is now php only

--- blocks/blocks.php
+++ blocks/blocks.php
@@ -8,9 +8,84 @@ add_action('init', __NAMESPACE__ . '\blockInit');
 function blockInit()
 {
     register_block_type(
-        __DIR__ . '/statistics/build',
+        'tsjippy-statistics/show',
         array(
-            'render_callback' => __NAMESPACE__ . '\statisticsWidget',
+            'title'           => __( 'Post Statistics', 'tsjippy' ),
+            'render_callback' => __NAMESPACE__.'\postStatistics',
+            'supports'        => array(
+                'autoRegister' => true,
+            ),
         )
     );
+}
+
+function postStatistics()
+{
+    $viewRoles     = SETTINGS['view-rights'] ?? [];
+    $userRoles     = wp_get_current_user()->roles;
+
+    //only continue if we have the right so see the statistics
+    if (!array_intersect($viewRoles, $userRoles)) {
+        return '';
+    }
+
+    global $wpdb;
+
+    $tableName  = $wpdb->prefix . 'tsjippy_statistics';
+    $url        = str_replace(TSJIPPY\SITEURL, '', TSJIPPY\currentUrl());
+
+    $pageViews  = $wpdb->get_results("SELECT * FROM $tableName WHERE url='$url' ORDER BY $tableName.`time_last_edited` DESC");
+
+    $totalViews             = 0;
+    $uniqueViewsLastMonths  = 0;
+    $now                    = new \DateTime();
+    foreach ($pageViews as $view) {
+        $totalViews += $view->counter;
+
+        $date = new \DateTime($view->time_last_edited);
+        $interval = $now->diff($date)->format('%m months');
+        if ($interval < 6) {
+            $uniqueViewsLastMonths++;
+        }
+    }
+    $uniqueViews   = count($pageViews);
+
+    ob_start();
+    ?>
+    <div class='pagestatistics'>
+        <h4>
+            Page statistics
+        </h4>
+        <table class='statistics-table'>
+            <tbody>
+                <tr>
+                    <td>
+                        <strong>Total views:</strong>
+                    </td>
+                    <td class='value'>
+                        <?php echo esc_attr($totalViews); ?>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <strong>Unique views:</strong>
+                    </td>
+                    <td class='value'>
+                        <?php echo esc_attr($uniqueViews); ?>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <strong>Unique views last 6 months:</strong>
+                    </td>
+                    <td class='value'>
+                        <?php echo esc_attr($uniqueViewsLastMonths); ?>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <?php
+
+    return ob_get_clean();
 }
--- blocks/statistics/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'ab026b97ee5baf7368fa');

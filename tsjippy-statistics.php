aec88e53aaa992fcfd793cbe61f53a3ac4868ef4
Tsjippy <ewaldha@hotmail.com>
2026-09-12 07:25:24 +0200

    changed: removed 'tested up to'

--- tsjippy-statistics.php
+++ tsjippy-statistics.php
@@ -10,10 +10,9 @@ namespace TSJIPPY\STATISTICS;
  * AuthorURI:            harmseninnigeria.nl
  * Requires at least:    6.3
  * Requires PHP:         8.3
- * Tested up to:         7.1
- * Plugin URI:            https://github.com/Tsjippy/statistics
+ * Plugin URI:           https://github.com/Tsjippy/statistics
  * Tested:               7.1
- * TextDomain:            tsjippy
+ * TextDomain:           tsjippy
  * Requires Plugins:    
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
